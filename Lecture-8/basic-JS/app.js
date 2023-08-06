0f1bb7e32ae4b9468f5cbe65e413c58623652b48
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-08-07 02:10:37 +0530

    lecture 8 codes

---
+++ Lecture-8/basic-JS/app.js
@@ -0,0 +1,56 @@
+// let tuntunmausi = "laddoo";
+// console.log(tuntunmausi);
+
+// tuntunmausi=false
+// console.log(tuntunmausi);
+
+
+// tuntunmausi=100
+// console.log(tuntunmausi);
+
+// // console.log(10)
+
+// let haarkejeetnewaalekojokerkehtehai = 100;
+
+
+// -------------------------------
+
+
+// let var_naam1 = 'samarth'
+// let var_naam2 = "samarth"
+// let var_naam3 = `samarth`
+
+// console.log(var_naam1)
+// console.log(var_naam2)
+// console.log(var_naam3)
+
+
+// -----------------------------
+
+
+
+let pehlaNaam = 'samarth'
+let aakhriNaam = 'vohra'
+let space = ' '
+
+
+// concatination
+
+// let pooraNaam2 = pehlaNaam + aakhriNaam;
+
+// let pooraNaam = pehlaNaam + space +aakhriNaam;
+
+// let pooraNaam = pehlaNaam + ' ' + aakhriNaam;
+
+
+// let pooraNaam = 'my first name is' + ' ' + pehlaNaam + ' ' + 'and my last name is' + ' ' + aakhriNaam;
+
+
+let pooraNaam = `my firstname is ${pehlaNaam} and my last name is ${aakhriNaam}`
+
+// let pooraNaam = 'my firstname is ${pehlaNaam} and my last name is ${aakhriNaam}'
+
+
+console.log(pooraNaam)
+
+
---
+++ Lecture-8/number/app.js
@@ -0,0 +1,62 @@
+
+
+// let magicNumber = 67;
+// console.log(magicNumber)
+
+
+// magicNumber = -21;
+// console.log(magicNumber)
+
+// magicNumber = 26.87;
+// console.log(magicNumber)
+
+// let str = 'samarth';
+
+// // let typebata = typeof(magicNumber);
+// let typebata = typeof(str);
+// console.log(typebata)
+
+// console.log(typeof(100))
+
+
+// -------------------------------
+
+// let a = 10;
+// let b = 20;
+
+// let c = a+b
+// let c = a-b
+// let c = a*b
+// let c = a/b
+// console.log(c)
+
+
+// let c = 33
+// let d = 10
+
+// let ans = c/d;
+
+// console.log(ans)
+
+// arithmatic -> BODMAS ❌
+
+// rhulzzz -> PEMDAS
+
+
+
+// let ans2 = (22 - 22 * 4) / (10/2) - 44 + 44
+// console.log(ans2)
+
+// let ans3 = -66 / 5 - 0
+// console.log(ans3)
+
+// let ans4 = parseInt(12.0003);
+// console.log(ans4)
+
+// ----------------------------
+
+
+let a = 5
+let b = 2
+
+console.log(a**b);
