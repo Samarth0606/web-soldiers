0250dfff745d1fec397df905792f07dc27342a1d
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-09-03 19:04:31 +0530

    lecture 15 codes

---
+++ Lecture-15/closures/app.js
@@ -0,0 +1,41 @@
+
+// console.log(a);
+// let a = 10;
+// function b(){
+//     console.log(a);
+//     function c(){
+//         console.log(a)
+//     }
+//     c()
+// }
+
+// b();
+
+
+// ------------------------
+
+// let a = 10;
+// function b(){
+//     console.log(a);
+//     function c(){
+//         console.log(a)
+//     }
+//     return c;
+// }
+
+// let c = b(); 
+
+// a=100; 
+
+// c();
+
+// console.log(a);
+
+
+
+
+
+
+
+
+
