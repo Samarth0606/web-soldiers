a75dba95b2796ca198283904e2aa71470369d021
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-10-14 18:43:47 +0530

    lecture 24 codes

---
+++ Lecture-24/jQuery/app.js
@@ -0,0 +1,48 @@
+let h11 = $('h1');
+let h12 = $('.samarth');
+let h13 = $('#sam');
+
+// --------------------------
+
+// h11.css('color' , 'red');
+// h11.css('background-color' , 'green');
+// h11.css('font-size' , '44px');
+
+
+// h11.css({
+//     color:'purple',
+//     backgroundColor: 'yellow',
+//     fontSize:'44px'
+// })
+
+// ----------------------------
+
+
+// .text()
+
+// $('p').text();
+
+
+// console.log($('p').text()) //getter
+
+// console.log($('p').text('hi samarth')) //setter
+// console.log($('p').text('<h1>hi samarth</h1>')) //wrong
+
+// .html()
+// console.log($('p').html()) //getter
+// console.log($('p').html('<h1>hi samarth</h1>')) //setter right
+
+
+// -----------------------------
+
+// attr()
+
+// let ans = $('#inp').attr('type') //getter
+// console.log(ans) 
+
+
+// let ans = $('#inp').attr('type' , 'color') //setter
+// let ans = $('#inp').attr('type' , 'date') //setter
+let ans = $('#inp').attr('type' , 'password') //setter
+
+
