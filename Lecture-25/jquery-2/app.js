280c3dacb5602b9a9247e2dd2d5f41045aba3c85
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-10-15 19:00:27 +0530

    lecture 25 codes

---
+++ Lecture-25/jquery-2/app.js
@@ -0,0 +1,87 @@
+// let h1 = $('h1')
+
+// let inp = $('#inp')
+
+// ------> add class
+// h1.first().addClass('first');
+// h1.first().addClass('first' , 'second');
+// h1.first().addClass('first second third');
+
+//-------> remove class
+// h1.first().removeClass('second')
+// h1.first().removeClass('second first')
+
+
+// -----> toggle class
+// h1.first().toggleClass('first second')
+
+// ------> has Class
+// let ans = h1.first().hasClass('first')
+// console.log(h1.first().hasClass('first'))
+// console.log(h1.first().hasClass('first second')) //order will matter
+
+
+// --------------------------------------
+
+
+$('button').click(function(){
+    console.log('clicked')
+})
+
+// $('li').click(function(e){
+//     // $('li').text()
+//     console.log(e.target.html())
+//     // console.log(e.target.text)
+// })
+
+
+// $('li').click(function(e){
+//     // $(this).text() ;
+//     console.log($(this).text()) ;
+//     // $(this).css('font-size' , '50px')
+// })
+
+
+// ----------------
+// $('li').click(function(){
+//     console.log($(this).text())
+//     $(this).css('background-color' , 'green')
+// })
+
+
+// $('li').click(function(){
+//     $(this).css('color' , 'red')
+// })
+
+// -------------
+
+// $('input').keyup(function(){
+//     console.log($(this).val());
+// })
+
+// $('input').keydown(function(){
+//     console.log($(this).val());
+// })
+
+// ----------------
+// addEventlistener() alternative
+// $('button').on('click' , function(){
+    // console.log('heelo')
+// })
+
+
+// ----------------------
+
+// $('#fadeIn').click(function(){
+//     // $('#divv').fadeIn();
+//     $('#divv').slideDown();
+// })
+// $('#fadeOut').click(function(){
+//     // $('#divv').fadeOut()
+//     $('#divv').slideUp()
+// })
+// $('#fadeToggle').click(function(){
+//     $('#divv').fadeToggle()
+// })
+
+
