57210835e988aac98e709befff6ec2c40f2699c1
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-11-26 18:55:05 +0530

    lecture 35 cods

--- Lecture-34/restful-architecture/index.js
+++ Lecture-34/restful-architecture/index.js
@@ -2,24 +2,31 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const methodOverride = require('method-override')
+
+const { v4: uuid } = require('uuid');
+
+app.use(methodOverride('_method'))
+
+
 let comments = [
     {
-        id : 0 , 
+        id : uuid() , 
         author : "Samarth",
         comment: "backend is easy"
     },
     {
-        id : 1 , 
+        id : uuid() , 
         author : "Priyanshu",
         comment: "kavita meri didi hai"
     },
     {
-        id : 2 , 
+        id : uuid() , 
         author : "Harshil",
         comment: "one note nhi dikhra"
     },
     {
-        id : 3 , 
+        id : uuid() , 
         author : "Tavneet",
         comment: "populate samjha do"
     },
@@ -51,16 +58,42 @@ app.get('/blog/new' , (req,res)=>{
 app.post('/blogs' , (req,res)=>{
     // console.log(req.body);
     let {author , comment} = req.body;
-    comments.push({author , comment , id: comments.length })
+    comments.push({author , comment , id: uuid() })
     res.redirect('/blogs');
 })
 
 // to show a particular blog
 app.get('/blogs/:id' , (req,res)=>{
     let {id} = req.params;
-    let foundProduct = comments.find((item)=>item.id === parseInt(id) );
+    // let foundProduct = comments.find((item)=>item.id === parseInt(id) );
+    let foundProduct = comments.find((item)=>item.id === id );
     // console.log(foundProduct)
     res.render('show' , {foundProduct})
+})
+
+// form to edit
+
+app.get('/blogs/:id/edit' , (req,res)=>{
+    let {id} = req.params;
+    let foundProduct = comments.find((item)=> item.id === parseInt(id) );
+    res.render('edit' , {foundProduct});
+})
+
+// to actually edit in db
+app.patch('/blogs/:id' , (req,res)=>{
+    let {id} = req.params;
+    let {comment} = req.body;
+    // let foundProduct = comments.find((item)=> item.id === parseInt(id) );
+    let foundProduct = comments.find((item)=> item.id === id );
+    foundProduct.comment = comment;
+    res.redirect('/blogs');
+})
+
+//to delete from db
+app.delete('/blogs/:id' , (req,res)=>{
+    let {id} = req.params;
+    comments = comments.filter((item)=>{return item.id != id});
+    res.redirect('/blogs');
 })
 
 
