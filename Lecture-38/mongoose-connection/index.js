ad24518d2b3bb0eb956950098afbd8bf1753eff9
macbook <macbook@Samarth-MacBook-Pro.local>
2023-12-16 19:15:54 +0530

    lecture 38 codes

---
+++ Lecture-38/mongoose-connection/index.js
@@ -0,0 +1,26 @@
+
+
+
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/cheerz')
+.then(()=>{console.log("DB connected")})
+.catch((err)=>{console.log(err)})
+
+
+let PORT = 8080;
+
+app.listen(PORT , ()=>{
+    console.log(`server conected at port: ${PORT}`);
+})
+
+
+
+
+
+
+
+
